eb15d80809186dc3096600f6d5cfae08b43530d8
Neeraj K Mondal <neeraj.mondal@outlook.com>
2019-12-07 22:26:37 +0530

    Added Get Template

--- GetTemplates/index.js
+++ GetTemplates/index.js
@@ -1,16 +1,60 @@
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+var request = require("request");
+var adal = require("adal-node");
+var fs = require("fs");
 
-    if (req.query.name || (req.body && req.body.name)) {
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
-        };
+
+module.exports = function (context, req) {
+
+    var authorityHostUrl = 'https://login.microsoftonline.com';
+    var tenant = ''; //'docsnode.com';
+    var resource = '';
+    if (req.body && req.body.tenant && req.body.SPOUrl) {
+        resource = req.body.SPOUrl;
+        tenant = req.body.tenant;
     }
-    else {
-        context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
+
+    var authorityUrl = authorityHostUrl + '/' + tenant;
+
+    //var resource = 'https://docsnode.sharepoint.com';
+
+
+    var certificate = fs.readFileSync(__dirname + '/devcert.pem', {
+        encoding: 'utf8'
+    });
+    var clientId = process.env['Dev-AD-APP-ClientID'];
+    var thumbprint = process.env['Dev-Cert-Thumbprint'];
+
+    var authContext = new adal.AuthenticationContext(authorityUrl);
+
+    authContext.acquireTokenWithClientCertificate(resource, clientId, certificate, thumbprint, function (err, tokenResponse) {
+        if (err) {
+            context.log('well that didn\'t work: ' + err.stack);
+            context.done();
+            return;
+        }
+        context.log(tokenResponse);
+
+        var accesstoken = tokenResponse.accessToken;
+
+        var options = {
+            method: "GET",
+            uri: resource + "/sites/DocsNodeAdmin/_api/web/lists/getbytitle('DocsNodeTemplatesLibrary')/items?$select=ID,Editor/Name,Editor/Title,LinkFilename,ContentTypeId,ContentType/Id,ContentType/Name,*&$expand=File,ContentType,Editor/Id&$top=5000",
+            headers: {
+                'Authorization': 'Bearer ' + accesstoken,
+                'Accept': 'application/json; odata=verbose',
+                'Content-Type': 'application/json; odata=verbose'
+            }
         };
-    }
+
+
+        context.log(options);
+        request(options, function (error, res, body) {
+            context.log(error);
+            context.log(body);
+            context.res = {
+                body: body || ''
+            };
+            context.done();
+        });
+    });
 };
